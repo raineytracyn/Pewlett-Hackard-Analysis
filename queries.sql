f5eeb8c501b930013b8435e378baccdb9e5097e1
raineytracyn <rainey.tracyn@gmail.com>
2021-01-30 19:23:57 -0500

    Hmwk

---
+++ queries.sql
@@ -0,0 +1,61 @@
+-- Creating tables for PH-EmployeeDB
+
+CREATE TABLE Departments (
+     dept_no VARCHAR(4) NOT NULL,
+     dept_name VARCHAR(40) NOT NULL,
+     PRIMARY KEY (dept_no),
+     UNIQUE (dept_name)
+);
+SELECT * FROM Departments;
+
+CREATE TABLE Employees (
+	 emp_no INT NOT NULL,
+     birth_date DATE NOT NULL,
+     first_name VARCHAR NOT NULL,
+     last_name VARCHAR NOT NULL,
+     gender VARCHAR NOT NULL,
+     hire_date DATE NOT NULL,
+     PRIMARY KEY (emp_no)
+);
+SELECT * FROM Employees;
+
+CREATE TABLE Dept_Manager (
+	dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+    PRIMARY KEY (emp_no, dept_no)
+);
+SELECT * FROM Dept_Manager;
+
+FOREIGN KEY (emp_no) REFERENCES Employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES Departments (dept_no),
+
+CREATE TABLE Salaries (
+  emp_no INT NOT NULL,
+  salary INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  PRIMARY KEY (emp_no)
+);
+SELECT * FROM Salaries;
+
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+
+CREATE TABLE Dept_Emp (
+  dept_no VARCHAR(4) NOT NULL,
+  emp_no INT NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  PRIMARY KEY (dept_no)
+);
+SELECT * FROM Dept_Emp;
+
+CREATE TABLE Titles (
+  emp_no INT NOT NULL,
+  title VARCHAR NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  PRIMARY KEY (emp_no)
+);
+SELECT * FROM Titles;
